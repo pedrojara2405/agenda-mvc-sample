96cd6917412d9b40b927320ef361c9d2b5197486
Pedro Jara <pedrojara2405@gmail.com>
2018-10-10 08:39:29 -0500

    Revert "Removing Models folder"
    This reverts commit b5709f2ea8bdaacd1b6b00aedf08f80e7fabb0e9.

---
+++ AgendaMVC/Controllers/PersonasController.cs
@@ -0,0 +1,44 @@
+﻿using Agenda.Logic;
+using Agenda.ViewModels;
+using System.Web.Mvc;
+
+namespace AgendaMVC.Controllers
+{
+    public class PersonasController : Controller
+    {
+        private LogicaPersonas _logicaPersonas;
+
+        public PersonasController()
+        {
+            _logicaPersonas = new LogicaPersonas();
+        }
+        public ActionResult Detalles(int id)
+        {
+            Agenda.Entities.Persona entidadPersonas = _logicaPersonas.ObtienePersona(id);
+            PersonaViewModel personaVm = new PersonaViewModel()
+            {
+                Id = entidadPersonas.Id,
+                Nombre = entidadPersonas.Nombre,
+                Numero = entidadPersonas.Numero
+            };
+            return View(personaVm);
+        }
+
+        public ActionResult Crear()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Crear(CreaPersonaViewModel personaNueva)
+        {
+            Agenda.Entities.Persona entidadPersona = new Agenda.Entities.Persona
+            {
+                Nombre = personaNueva.Nombre,
+                Numero = personaNueva.Numero
+            };
+            _logicaPersonas.InsertaPersona(entidadPersona);
+            return RedirectToAction("Detalles", new { id = entidadPersona.Id });
+        }
+    }
+}
